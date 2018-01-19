eaa8123396c364249b63f372739cb03b455d93e8
brapls <brianderg@gmail.com>
2018-01-19 14:09:42 +0100

    1
    1

---
+++ Highscore/src/com/company/HighScore.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/**
+ * Created by brian on 19.01.2018.
+ */
+public class HighScore {
+}
---
+++ Highscore/src/com/company/HighScoreAdmin.java
@@ -0,0 +1,19 @@
+package com.company;
+
+/**
+ * Created by brian on 19.01.2018.
+ */
+public class HighScoreAdmin {
+    String submit(String name, String dateTime, String level, int finishTime){
+        return "";
+    }
+    HighScore[] getHighScored(String level){
+
+    }
+
+    void save(){}
+
+    void load(){
+
+    }
+}
