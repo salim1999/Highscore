eef515fef0ef97ee763d5bc7b18894ae4b041441
salim1999 <33951489+salim1999@users.noreply.github.com>
2018-01-19 14:38:12 +0100

    tt

--- Highscore/src/com/company/HighScoreAdmin.java
+++ Highscore/src/com/company/HighScoreAdmin.java
@@ -1,5 +1,10 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Created by brian on 19.01.2018.
  */
@@ -8,12 +13,45 @@ public class HighScoreAdmin {
         return "";
     }
     HighScore[] getHighScored(String level){
-
+return null;
     }
 
     void save(){}
 
-    void load(){
+  public String[]  load(String Path){
+
+        String csvFile = Path;
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+      String[] Statistic = new String[101];
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                 Statistic = line.split(cvsSplitBy);
+
+
+
+            }
+            return Statistic;
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
 
     }
 }
